ca96e51da70a186609e0bda0ad4770aba03e8939
Ruslan <sweetshima18@gmail.com>
2021-05-28 14:44:40 +0300

    Add support module

--- JavaScript/SimpleTabs/js/index.js
+++ JavaScript/SimpleTabs/js/index.js
@@ -2,44 +2,48 @@ window.addEventListener('DOMContentLoaded', function () {
 
   'use strict';
 
-  // Node elements of our page:
-  const $tabsWrapper = document.querySelector('.some-wrapper'),
-    $tabs = document.querySelectorAll('.some-tabs'),
-    $tabsContent = document.querySelectorAll('.some-tabs-content');
-  
-  // Hiding tabs content
-  function hideTabsContent(index) {
-    for (let i = index; i < $tabsContent.length; i++) {
-      $tabsContent[i].classList.remove('show');
-      $tabsContent[i].classList.add('hide');
+  function simpleTabs() {
+    // Node elements of our page:
+    const $tabsWrapper = document.querySelector('.some-wrapper'),
+      $tabs = document.querySelectorAll('.some-tabs'),
+      $tabsContent = document.querySelectorAll('.some-tabs-content');
 
-      // Without classes :
-      // $tabsContent[i].style.display = 'none';
-      // $tabsContent[i].style.display = 'flex';
-    }
-  }
-  hideTabsContent(1);
+    // Hiding tabs content
+    function hideTabsContent(index) {
+      for (let i = index; i < $tabsContent.length; i++) {
+        $tabsContent[i].classList.remove('show');
+        $tabsContent[i].classList.add('hide');
 
-  // Showing tabs content
-  function showTabsContent(index) {
-    if ($tabsContent[index].classList.contains('hide')) {
-      $tabsContent[index].classList.remove('hide');
-      $tabsContent[index].classList.add('show');
-    }
-  }
-
-  // Event listener for clicking on tabs. Used: Delegating events.
-  $tabsWrapper.addEventListener('click', (event) => {
-    const target = event.target;
-
-    if (target && target.matches('div.some-tabs')) {
-      for (let i = 0; i < $tabs.length; i++) {
-        if (target == $tabs[i]) {
-          hideTabsContent(0);
-          showTabsContent(i);
-          break;
-        }
+        // Without classes :
+        // $tabsContent[i].style.display = 'none';
+        // $tabsContent[i].style.display = 'flex';
       }
     }
-  });
+    hideTabsContent(1);
+
+    // Showing tabs content
+    function showTabsContent(index) {
+      if ($tabsContent[index].classList.contains('hide')) {
+        $tabsContent[index].classList.remove('hide');
+        $tabsContent[index].classList.add('show');
+      }
+    }
+
+    // Event listener for clicking on tabs. Used: Delegating events.
+    $tabsWrapper.addEventListener('click', (event) => {
+      const target = event.target;
+
+      if (target && target.matches('div.some-tabs')) {
+        for (let i = 0; i < $tabs.length; i++) {
+          if (target == $tabs[i]) {
+            hideTabsContent(0);
+            showTabsContent(i);
+            break;
+          }
+        }
+      }
+    });
+  }
+
+  module.exports = simpleTabs;
 });
